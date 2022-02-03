17a9120eb540037e8802bde0443cbb9cfb2b8531
pownas <pownas@outlook.com>
2022-02-03 23:18:52 +0100

    Justerat index-sidan med year och leapYear
     leapYear ? "är skottår" : "inte är skottår";

--- js/scriptet.js
+++ js/scriptet.js
@@ -394,6 +394,9 @@ switch (dayOfYear) {
 // Skriver ut det case som blev ovan som dagens namnsdag i index-filens DOM. 
 document.getElementById("dagensDatum").innerHTML = datumet;
 document.getElementById("dagensNamnsdagar").innerHTML = namnen;
+document.getElementById("dagensYear").innerHTML = year;
+document.getElementById("dagensLeapYear").innerHTML = leapYear ? "är skottår" : "inte är skottår";
+
 
 //Kollar om elementet med ID "dagPåÅret finns, skriver annars ut i konsollen att det inte finns. "
 try{
